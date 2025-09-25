fc426f8fe4caa109340ece83935eb030795794b9
Jonathan Perez <jonathanmperez1226@gmail.com>
2025-09-25 17:57:14 +0200

    Add files via upload

---
+++ PrimerDia/edad.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class edad {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Pon tu nota: ");
+        double nota = sc.nextDouble();
+
+        if (nota < 5) {
+            System.out.println("Has suspendido");
+        } else if (nota < 6) {
+            System.out.println("Suficiente");
+        } else if (nota < 7) {
+            System.out.println("Bien");
+        } else if (nota < 9) {
+            System.out.println("Notable");
+        } else if (nota <= 10) {
+            System.out.println("exelente");
+        } else {
+            System.out.println("Nota no válida");
+        }
+
+        sc.close();
+    }
+}
