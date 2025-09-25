9137ddaa1951d317d1af5ed7f8cc33b626a7f860
Sergi <145382808+Lasoro24@users.noreply.github.com>
2025-09-25 17:32:11 +0200

    Add files via upload

---
+++ PrimerDia/miniEx.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class miniEx {
+    public static void main(String[] args) { 
+        Scanner sc = new Scanner(System.in);
+
+        double nota;
+        nota = sc.nextDouble();
+
+        if (nota <= 4.99) {
+            System.out.println("Suspenso");
+        } 
+        else if (nota >= 5 && nota < 5.99) {
+            System.out.println("Aprovado");
+        } 
+        else if (nota >= 6 && nota < 8.99) {
+            System.out.println("Notable");
+        } 
+        else if (nota >= 9 && nota <= 10) {
+            System.out.println("Excelente");
+        } 
+        else {
+            System.out.println("Nota invlida");
+        }
+        sc.close();
+    }
+}
