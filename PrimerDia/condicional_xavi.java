34b572b5ae9800ac8a97fc9b6947fbd191de7e95
bonbon277 <bonbonfizzifaz@gmail.com>
2025-09-25 17:23:38 +0200

    Add files via upload

---
+++ PrimerDia/condicional_xavi.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class condicional {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double nota;
+        nota = sc.nextInt();
+        if (nota >= 0 && nota < 4.99) {
+            System.out.println("deficiente mental");
+        } else if (nota >= 5 && nota < 6.99) {
+            System.out.println("suficiente");
+        }else if (nota >= 7 && nota < 8.99) {
+            System.out.println("notable");
+        }else if (nota >= 9 && nota <= 10) {
+            System.out.println("sobresaliente");
+        }else{
+            System.out.println("nota no valida");
+        }
+        
+    }
+}
