e1980a3c2e2171a6795374d823665724e6392c84
Irsis Sánchez <irsissosasanchez@gmail.com>
2025-09-28 16:15:12 +0200

    Add files via upload

---
+++ 20250925/Ex7IrsisS.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class Ex7IrsisS {
+
+ public static void main(String[] args)  {
+       
+
+//Programa que llegeix tres números diferents i ens diu quin és el més gran.
+ //Declarar variables: valor1, valor2, valor3.
+
+Scanner sc = new Scanner(System.in);
+double valor1, valor2, valor3;
+
+System.out.println("Afegeix un número: ");
+valor1 = sc.nextDouble();
+
+System.out.print("Afegeix un segon número: ");
+valor2 = sc.nextDouble();
+System.out.print("Afegeix un tercer número: ");
+valor3 = sc.nextDouble();
+
+   // si el valor1 és més gran que el valor2 i valor3:
+   if(valor1>valor2 && valor1>valor3)
+   {
+    System.out.println("El valor més gran és: " + valor1);
+   }
+   // si el valor2 és més gran que el valor1 i el valor3:
+   if (valor1<valor2 && valor2>valor3)
+   {
+    System.out.println("El valor més gran és: " + valor2);
+   }
+   // si el valor3 és més gran que el valor1 i el valor2:
+   if (valor1>valor2 && valor1<valor3)
+   {
+    System.out.println("El valor més gran és: " + valor3);
+   }
+
+}
+}
