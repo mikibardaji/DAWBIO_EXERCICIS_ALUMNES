650c11a0230f773297c6c92fbd853438e5c49dec
samuelpixel2 <154022007+samuelpixel2@users.noreply.github.com>
2025-09-29 15:29:04 +0200

    Create samuel_ex5.java

---
+++ 20250925/samuel_ex5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Introduce el primer número: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Introduce el segundo número: ");
+        int num2 = sc.nextInt();
+
+        if (num1 < num2) {
+            System.out.println(num1 + " " + num2);
+        } else {
+            System.out.println(num2 + " " + num1);
+        }
+
+    }
+}
