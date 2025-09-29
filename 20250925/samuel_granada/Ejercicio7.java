716595489abb2be3745a349f68abca628ac03273
samuelpixel2 <154022007+samuelpixel2@users.noreply.github.com>
2025-09-29 15:35:04 +0200

    Add files via upload

---
+++ 20250925/samuel_granada/Ejercicio7.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Introduce el primer número: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Introduce el segundo número: ");
+        int num2 = sc.nextInt();
+
+        System.out.print("Introduce el tercer número: ");
+        int num3 = sc.nextInt();
+
+        if (num1 > num2 && num1 > num3) {
+            System.out.println("El mayor es: " + num1);
+        } else if (num2 > num1 && num2 > num3) {
+            System.out.println("El mayor es: " + num2);
+        } else {
+            System.out.println("El mayor es: " + num3);
+        }
+    }
+}
