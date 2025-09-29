097e51882334269533f6b32ffac6d26f29897284
Irsis Sánchez <irsissosasanchez@gmail.com>
2025-09-29 15:54:58 +0200

    Ex7IrsisS.java

--- 20250925/Irsis/Ex7IrsisS.java
+++ 20250925/Irsis/Ex7IrsisS.java
@@ -9,30 +9,32 @@ public class Ex7IrsisS {
 
 Scanner sc = new Scanner(System.in);
 double valor1, valor2, valor3;
-
+  
+//entrada de datos
 System.out.println("Afegeix un número: ");
 valor1 = sc.nextDouble();
-
 System.out.print("Afegeix un segon número: ");
 valor2 = sc.nextDouble();
 System.out.print("Afegeix un tercer número: ");
 valor3 = sc.nextDouble();
-
+  
+//procesar datos
    // si el valor1 és més gran que el valor2 i valor3:
-   if(valor1>valor2 && valor1>valor3)
+   if(valor1>=valor2 && valor1>=valor3)
    {
     System.out.println("El valor més gran és: " + valor1);
    }
    // si el valor2 és més gran que el valor1 i el valor3:
-   if (valor1<valor2 && valor2>valor3)
+   if (valor1<=valor2 && valor2>=valor3)
    {
     System.out.println("El valor més gran és: " + valor2);
    }
    // si el valor3 és més gran que el valor1 i el valor2:
-   if (valor1>valor2 && valor1<valor3)
+   if (valor1>=valor2 && valor1<=valor3)
    {
     System.out.println("El valor més gran és: " + valor3);
    }
 
 }
 }
+
