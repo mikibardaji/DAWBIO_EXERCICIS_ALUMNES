bcd60f057654ee7f22a513e2342b22598840bfe1
Jonathan Perez <jonathanmperez1226@gmail.com>
2025-09-29 16:34:14 +0200

    Add files via upload

---
+++ 20250925/jonathanPeerez/exercicio7.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class exercicio7 {
+ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1;
+        int num2;
+        int num3;
+
+        System.out.println("introduce num1");
+        num1 = sc.nextInt();
+        System.out.println("introduce num2");
+        num2 = sc.nextInt();
+        System.out.println("introduce num3");
+        num3 = sc.nextInt();
+        if (num1 > num2 && num1 > num3) {
+            System.out.println(num1);
+        } else if (num2 > num1 && num2 > num3) {
+            System.out.println(num2);
+        } else if (num3 > num1 && num3 > num2) {
+            System.out.println(num3);
+        } else {
+            System.out.println("son iguales");
+        }
+
+    }
+}
