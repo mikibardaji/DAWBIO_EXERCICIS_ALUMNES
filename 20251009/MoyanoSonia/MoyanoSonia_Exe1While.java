d5bb640a2fcf8b01bc3dcc73e190a3c90319b55c
RedSorsha <jovenlector@gmail.com>
2025-10-09 23:35:09 +0200

    Update MoyanoSonia_Exe1While.java

--- 20251009/MoyanoSonia/MoyanoSonia_Exe1While.java
+++ 20251009/MoyanoSonia/MoyanoSonia_Exe1While.java
@@ -8,28 +8,27 @@ public static void main(String[] args) throws Exception {
 int mostra=0;
 System.out.println("Observant mostres......");
 
-    //definirem un bucle 
+//definirem un bucle mentre la mostra no sigui 10 s'anirá executant quan sigui falsa la condició sortirà del bucle
         
 do{
 
-System.out.println("Mostrant glòbul:"+(mostra+1));
+        System.out.println("Mostrant glòbul:"+(mostra+1));
 
-mostra++;
+        mostra++;
 
 }while (mostra!=10);
 
 
 //una vegada fora del bucle analitzarem les mostres des de la última observada 
-    System.out.println("Processant les mostres des de la última observada....");
-//a la primera, per això descomptarem el comptador começant pel 10 fins que el comptador sigui major a 0 quan arribi a 0 finalitzarà
+System.out.println("Processant les mostres des de la última observada....");
+//per això descomptarem el comptador começant pel 10 fins que el comptador mentre no sigui 0 quan aixó no es compleixi finalitzarà
 do{
 
-System.out.println("Glòbul "+(mostra)+ " ja s'ha processat");
+        System.out.println("Glòbul "+(mostra)+ " ja s'ha processat");
 
-mostra--;
+        mostra--;
 
 }while (mostra!=0);
 
     }
 }
-
