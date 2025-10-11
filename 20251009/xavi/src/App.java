d6fcee2d2060b5f60dcaf0845cc6da3cfaede244
bonbon277 <bonbonfizzifaz@gmail.com>
2025-10-11 16:34:28 +0200

    Add files via upload

---
+++ 20251009/xavi/src/App.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        for(int i=0; i<10 ;i++){
+            System.out.println("Mostrant glòbul"+i);
+        }
+    }
+}
