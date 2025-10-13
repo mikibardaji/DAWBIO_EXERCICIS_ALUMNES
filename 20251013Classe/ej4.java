1d724098ca76e4e5c9babc80040665b1f8bf2baa
claudia <claudialeon101010@gmail.com>
2025-10-13 16:30:27 +0200

    Add files via upload

---
+++ 20251013Classe/ej4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class ej4 {
+    public static void main(String[] args) throws Exception {
+    Scanner sc= new Scanner (System.in);
+    //Programa que entra un numero natural N i mostra els numeros parells entre 1 i N
+    //Pero amb un comptador sumant de 1 en 1
+    int N, contador;
+    System.out.print("Introdueix un numero natural:");
+    N=sc.nextInt();
+        for(contador=1; contador<=N; contador ++){
+            if (contador%2==0) {
+                System.out.println(contador);
+            }
+            }
+        
+}
+}
