d3fdb2bd36fab777183cdba95a14b93174510bd2
Daniela <applepi2k@gmail.com>
2025-10-13 16:36:58 +0200

    Update Ex3_nombre_parells.java

--- 20251013Classe/Daniela Chacha/Ex3_nombre_parells.java
+++ 20251013Classe/Daniela Chacha/Ex3_nombre_parells.java
@@ -7,16 +7,17 @@ public class Ex3_nombre_parells
         Scanner sc = new Scanner(System.in);
         int num_natural, contador;
 
-        System.out.println("Pon un númerto natural: ");
+        System.out.println("Pon un número natural: ");
         num_natural = sc.nextInt();
 
         for (contador=2; num_natural>=contador; contador++)
         {
             if (contador%2==0)
             {
-                System.out.println("Númerp parell: " + contador);
+                System.out.println("Número parell: " + contador);
             }
         }
     }
 
 }
+
