37e6305544823de22fcb5d643436d73bab99399a
zreduardo25-netizen <zreduardo25@gmail.com>
2025-10-13 16:45:48 +0200

    Add files via upload

---
+++ 20251013Classe/Eduardo_Rodriguez/Ejercicio_6_Eduardo_Rodriguez.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Ejercicio_6_Eduardo_Rodriguez {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int valor = 10, critic = 0;
+
+        for (int contador = 0; contador < 20; contador ++) {
+            System.out.println("Dime un Valor de presion arterial: ");
+            valor = sc.nextInt();
+            if (valor < 0) {
+                critic++;
+            }
+        }
+        if (critic>=1) {
+            System.out.println("Hay un valor negativo");
+            System.out.println("Cantidad de valores negativos: " + critic);
+        }
+        else {
+            System.out.println("Todos los valores son correctos");
+        }
+        
+    }
+}
