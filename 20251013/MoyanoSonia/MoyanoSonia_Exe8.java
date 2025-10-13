fbf83c900d181fb0ee7fb4f2007d8720e09ab448
RedSorsha <jovenlector@gmail.com>
2025-10-13 19:54:57 +0200

    Create MoyanoSonia_Exe8.java

---
+++ 20251013/MoyanoSonia/MoyanoSonia_Exe8.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class cadenaAT {
+    public static void main(String[] args) throws Exception {
+        
+    // 8.	Mostra per pantalla una cadena de 20 lletres alternant A i T (adenina i timina):
+    char a='A'; char t='T';
+
+    System.out.println("Imprimint cadena d'adenina i timina:");
+    
+    //realitzarem un for on imprimirem 10 vegades cada part de la cadena     
+    for  (int i=0; i<10; i++){
+    
+        System.out.print(a);
+        System.out.print(t);
+        
+    }
+  
+    }
+}
