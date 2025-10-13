a8dcdd22b4ef2fa8d5fcf43c581623c208262ca8
zreduardo25-netizen <zreduardo25@gmail.com>
2025-10-13 16:08:32 +0200

    Add files via upload

---
+++ 20251013Classe/Eduardo_Rodriguez/Ejercicio_5_Eduardo_Rodriguez.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ejercicio_4_Eduardo_Rodriguez {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int tiempo = 10;
+
+        System.out.println("\nPreparando quirofano");
+        while (tiempo>=1){
+            
+        System.out.println((tiempo));
+        tiempo--;
+        }
+        System.out.println("Iniciendo la intervencion");
+        
+    }
+}
