1e573aec312d8a8fd6b098c67bd23852039ef95b
Jonathan Perez <jonathanmperez1226@gmail.com>
2025-10-13 15:35:20 +0200

    Delete 20251009/jonathan.java directory

--- 20251009/jonathan.java/ExFor.java
+++
@@ -1,7 +0,0 @@
-public class ExFor {
-    public static void main(String[] args) {
-        for (int contador = 1; contador <= 10; contador++) {
-            System.out.println("Mostrando globulo " + contador);
-        }
-    }
-}
