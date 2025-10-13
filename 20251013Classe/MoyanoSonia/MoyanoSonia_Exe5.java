25d4f825522909ca9d8d13dc6fef505abe10cafb
RedSorsha <jovenlector@gmail.com>
2025-10-13 16:53:20 +0200

    Create MoyanoSonia_Exe5.java

---
+++ 20251013Classe/MoyanoSonia/MoyanoSonia_Exe5.java
@@ -0,0 +1,21 @@
+public class ComptadorQuir {
+
+ public static void main(String[] args) throws Exception {
+//Mostra un compte enrere de 10 a 1 abans d’una intervenció:
+//Preparant quiròfan...
+       
+int i;
+
+System.out.println("Preparant quiròfan....");
+    
+    for (i=10 ; i>0;i--){
+
+        System.out.println(i);
+    }
+
+
+System.out.println("Inici de la intervenció.");
+
+    }
+
+}
