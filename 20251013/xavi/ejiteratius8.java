87008c64251217c32da2b54964ac6b6f65438306
bonbon277 <bonbonfizzifaz@gmail.com>
2025-10-16 15:20:35 +0200

    Add files via upload

---
+++ 20251013/xavi/ejiteratius8.java
@@ -0,0 +1,19 @@
+
+public class ejiteratius8 {
+    public static void main(String[] args) {
+        char aminoacid='A';
+        for (int i=0; i<20;i++){
+            
+            if (i%2==0){
+                aminoacid='A';
+            }
+            else{
+                aminoacid='T';
+            }
+             System.out.print(aminoacid);
+        }
+      
+
+    
+}
+}
