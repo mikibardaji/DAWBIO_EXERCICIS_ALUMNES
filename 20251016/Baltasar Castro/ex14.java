4031a39e2282a2a851ea6fbbe988783652bb8e88
CRBalta <crbaltasar@gmail.com>
2025-10-20 15:24:46 +0200

    Add files via upload

---
+++ 20251016/Baltasar Castro/ex14.java
@@ -0,0 +1,37 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class ex14 {
+    public static void main(String[] args){
+        Random rnd = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int respuesta=0;
+        int respuestaia=0;
+        int min=1;
+        int max=100;
+        int pista;
+
+        System.out.println("dime el numero q adivinar la ia pendeja 0-100: ");
+        respuesta = scanner.nextInt();
+        do{ 
+            respuestaia = rnd.nextInt(min,max);
+            System.out.println("la ia pendeja a dicho: " + respuestaia);
+            System.err.println("mas alto o mas bajo? 1-mas alto 2-mas bajo");
+            pista = scanner.nextInt();
+            if (pista == 1) {
+                min = respuestaia + 1;
+                
+            }else if (pista == 2) {
+                max = respuestaia - 1;
+                
+            }
+        }while(respuestaia != respuesta);
+
+        System.out.println("la ia pendeja a adivinado tu numero: " + respuestaia);
+        System.out.println("la ia pendeja es la mejor");
+        System.err.println("ayuda la ia se esta haciendo una automamada yo no puse eso");
+       
+    }
+}
+
