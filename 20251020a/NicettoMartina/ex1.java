9f54c29c014f8c5b563bc7ce06ebfd4e8fd92cb1
MartinaNicetto <nicettomartina@gmail.com>
2025-10-30 16:44:49 +0100

    Refactor ex1.java to improve structure and readability

--- 20251020a/NicettoMartina/ex1.java
+++ 20251020a/NicettoMartina/ex1.java
@@ -3,23 +3,39 @@ import java.util.Scanner;
 public class ex1 {
 
     public static void main(String[] args) {
-       double  resultado= respuesta();
-        System.out.println("El producto es " + resultado);
-
-    }
-
-    private static double respuesta()
-    {
+       
         Scanner sc=new Scanner(System.in);
-        System.out.println("Dime un numero ");
-        double num1=sc.nextDouble();
-        System.out.println("Dime un numero ");
-        double num2=sc.nextDouble();
+        double resultado;
+        double num1,num2;
+        double n,N;
 
-       double producto=num1*num2;
-       return producto;
+        num1= pedirnumero(1);
+        num2=pedirnumero(2);
+       
 
+        resultado= producto(num1,num2);
+
+        System.out.println("El resultado es " + resultado);
     }
+
+    public static double producto(double x, double y)
+    { 
+       double multi=x*y;
+       return multi;
+    };
       
+    public static double pedirnumero(int x)
+    {
+        double n;
+        Scanner sc=new Scanner(System.in);
+        do{
+        System.out.println("Dime un numero "+ x);
+        n=sc.nextDouble();
+        }while(n<0); 
+        
+        return n;
+    
+    }
 
 }
+
