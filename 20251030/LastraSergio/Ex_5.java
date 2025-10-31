11fe6e9e287c2312092f37291a4e5e0402edc2b2
Sergi <145382808+Lasoro24@users.noreply.github.com>
2025-10-31 18:36:41 +0100

    Add files via upload

---
+++ 20251030/LastraSergio/Ex_5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Ex_5 {
+    public static void main(String[] args) {
+        double milles = (double)
+        millesAKm(0);
+        System.out.println("Milles convertides a Km: " + milles);
+    }
+
+    public static double millesAKm (double milles){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Cuantes milles vols convertir a Km? ");
+
+        milles = sc.nextInt();
+        sc.close();
+        
+        return milles * 1.60934;
+    }
+}
---
+++ 20251030/LastraSergio/Ex_6.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Ex_6 {
+    public static void main(String[] args) {
+        System.out.print("Introdueix un valor per x: ");
+        int x = num1(0);
+        System.out.print("Introdueix un valor per y: ");
+        int y = num2(0);
+        compara(x, y);
+    }
+
+    public static int num1(int x) {
+        Scanner sc = new Scanner(System.in);
+        x = sc.nextInt();
+        return x;
+    }
+
+    public static int num2(int y) {
+        Scanner sc = new Scanner(System.in);
+        y = sc.nextInt();
+        return y;
+    }
+
+    public static int compara(int x, int y){
+
+        int cmp = Integer.compare(x, y);
+
+        if (cmp < 0){
+            System.out.println(x + " Es menor que " + y);
+        }
+        else if (cmp > 0){
+            System.out.println(x + " Es mayor que " + y);
+        }
+        else {
+            System.out.println(x + " Es igual que " + y);
+        }
+
+        return cmp;
+    }
+}
