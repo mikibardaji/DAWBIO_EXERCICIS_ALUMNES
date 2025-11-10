7ebc82f872ab998b41572d5c93d9b92abfc5899b
zreduardo25-netizen <zreduardo25@gmail.com>
2025-11-10 16:59:52 +0100

    Add files via upload

---
+++ 20251106/Eduardo_Rodriguez-Thiago-Lafora/EX_1_Con_Boolean.java
@@ -0,0 +1,104 @@
+import java.util.Scanner;
+
+public class EX_1_Con_Boolean {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int puntos_Jug = 0;
+        int puntos_Ord = 0;
+
+        while (puntos_Jug < 3 && puntos_Ord < 3) {
+            int jugada_Jug = jugada();
+            int jugada_Ord = JugadaOrdenador();
+
+            imp_jugada("Jugador", jugada_Jug);
+            imp_jugada("Ordenador", jugada_Ord);
+
+            int resultado = ganador(jugada_Jug, jugada_Ord);
+            imp_ganador(resultado);
+
+            if (resultado == 1) {
+                puntos_Jug = aumentar_punto(puntos_Jug);
+            } else if (resultado == 2) {
+                puntos_Ord = aumentar_punto(puntos_Ord);
+            }
+
+            marcador(puntos_Jug, puntos_Ord);
+        }
+
+        sc.close();
+    }
+
+    //Eduardo
+    public static int JugadaOrdenador() {
+        int a = (int)(Math.random() * (2 - 0 + 1)) + 0; 
+        System.out.println();
+        return a;
+    }
+
+    //Eduardo
+    public static void imp_ganador (int ganador) {
+        switch (ganador) {
+            case 0:
+                System.out.println("Hay empate");
+                break;
+            case 1:
+                System.out.println("Has gando");
+                break;
+            case 2:
+                System.out.println("Ha ganado el ordenador");
+                break;
+        }
+    }
+
+    //Eduardo
+    public static int aumentar_punto(int score) {
+        score ++;
+        return score;
+    }
+
+    //Eduardo
+    public static int ganador(int choose_player1, int choose_player2) {
+
+        if (choose_player1 == choose_player2) {
+            return 0;
+        }
+        else if ((choose_player1==0 && choose_player2==2)||(choose_player1==1 && choose_player2==0)||(choose_player1==2 && choose_player2==1) ) {
+            return 1;
+        }
+        else {
+            return 2;
+        }
+    }
+
+    //thiago
+    public static void marcador(int point_player1, int point_player2) {
+        System.out.println("Jugador 1: " + point_player1 + " - Jugador 2: " + point_player2);  
+    }
+
+    //thiago
+    public static int jugada() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Elige 0-piedra, 1-papel o 2-tijera: ");
+        int eleccion = sc.nextInt();
+        return eleccion;
+    }
+
+    //Thiago
+    public static void imp_jugada(String quien_juega, int choose) {
+        String jugada_texto = "";
+        switch (choose) {
+            case 0:
+                jugada_texto = "Piedra";
+                break;
+            case 1:
+                jugada_texto = "Papel";
+                break;
+            case 2:
+                jugada_texto = "Tijera";
+                break;
+        }
+        System.out.println(quien_juega + ": " + jugada_texto);
+    }
+}
