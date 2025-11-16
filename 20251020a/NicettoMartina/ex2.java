9eee45eed4f74c9dcb0d4d3a7e38eb6e291ccd59
MartinaNicetto <nicettomartina@gmail.com>
2025-11-16 17:48:06 +0100

    Refactor age checking program for clarity

--- 20251020a/NicettoMartina/ex2.java
+++ 20251020a/NicettoMartina/ex2.java
@@ -1,45 +1,30 @@
 import java.util.Scanner;
 
 public class ex2 {
-    // Programa que demana a l’usuari una edat i indica si és major d’edat (edat major o igual a 18 anys).
-    // Ha d’implementar un mètode amb el prototip boolean esMajorEdat(int edat).
 
     public static void main(String[] args) {
-       
-      int edad;
-
-      edad=pedirNumeroEnteroPos();
-
-      boolean mayor=mayored(edad);
-      mostrar(edad);
-    }
-
- public static void mostrar(int edad){
-   System.out.println("Muestra la edad "+edad);
-
- }
-    public static boolean mayored(int edad) 
-    {
-        boolean mayor = true;  
-        if (edad>=18) {
-            System.out.println("Eres mayor de edad");
-            return mayor = true;
-        } else {
-            System.out.println("Eres menor de edad");
-            return mayor = false;
-        }
+        int edad=pidedad();
         
-    }
+        mayoredad(edad);
 
-    public static int pedirNumeroEnteroPos()
+    }
+    public static int pidedad(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Cual es tu edad: ");
+        int x=sc.nextInt();
+        return x;
+    }
+    public static boolean mayoredad(int x)
     {
-        int valor;
-         Scanner sc=new Scanner(System.in);
-         do{
-            System.out.println("Pon tu edad: ");
-            valor=sc.nextInt();
-         }while(valor<0);
-         return valor;
-
+     if(x<18)
+     {
+        System.out.println("Eres menor de edad");
     }
+    else if (x>=18)
+    {
+        System.out.println("Eres mayor de edad");
+    }
+    return true;
+    }
+     
 }
