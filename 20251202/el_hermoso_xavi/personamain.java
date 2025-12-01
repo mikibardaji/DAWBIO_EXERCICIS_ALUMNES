3955f0212170059495d1fb5184cf027668627a9a
bonbon277 <bonbonfizzifaz@gmail.com>
2025-12-01 15:40:17 +0100

    Add files via upload

---
+++ 20251202/el_hermoso_xavi/personamain.java
@@ -0,0 +1,52 @@
+package Objectes;
+import java.util.Scanner;
+import Objectes.persona;
+
+public class personamain {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Primera Persona");
+        System.out.print("Nombre: ");
+        String nom = sc.nextLine();
+
+        System.out.print("Apellidos: ");
+        String cognoms = sc.nextLine();
+
+        System.out.print("DNI: ");
+        String dni = sc.nextLine();
+
+        System.out.print("Edad: ");
+        int edat = sc.nextInt();
+        sc.nextLine();
+
+        persona persona1 = new persona(nom, cognoms, dni, edat);
+
+        System.out.println("Segunda persona");
+        System.out.print("Nombre: ");
+        nom = sc.nextLine();
+
+        System.out.print("Apellidos: ");
+        cognoms = sc.nextLine();
+
+        System.out.print("DNI: ");
+        dni = sc.nextLine();
+
+        System.out.print("Edad: ");
+        edat = sc.nextInt();
+        sc.nextLine();
+
+        persona persona2 = new persona(nom, cognoms, dni, edat);
+
+        System.out.println("Persones:: ");
+        persona1.mostrarPersona();
+        persona1.esMajorEdat(); 
+
+
+        persona2.mostrarPersona();
+        persona2.esMajorEdat();
+
+        System.out.println("Diferencia de edad: " + persona1.diferenciaedat(persona2));
+    }
+}
---
+++ 20251202/el_hermoso_xavi/puntmain.java
@@ -0,0 +1,19 @@
+package Objectes;
+public class puntmain {
+    public static void main(String[] args) {
+
+        punt p1 = new punt(5, 0);
+        punt p2 = new punt(10, 10);
+
+        System.out.println("--------");
+        p1.mostrarPunt();
+        p2.mostrarPunt();
+
+        p1.desplaza(3, -2);
+        System.out.println("p1:");
+        p1.mostrarPunt();
+
+        System.out.println("Distancia:");
+        System.out.println(p1.distancia(p2));
+    }
+}
