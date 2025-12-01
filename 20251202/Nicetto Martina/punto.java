36b3d617add72b0804e70505f655183cc5851db5
MartinaNicetto <nicettomartina@gmail.com>
2025-12-01 16:16:17 +0100

    Update punto.java

--- 20251202/Nicetto Martina/punto.java
+++ 20251202/Nicetto Martina/punto.java
@@ -9,8 +9,26 @@ public class punto {
         this.x = x;
         this.y = y;
     }
+    public void setx(int x)
+    {
+        this.x=x;
+    }
+    public void sety(int y)
+    {
+        this.y=y;
+    }
 
+    public int getx()
+    {
+        return x;
+    }
+    public int gety()
+    {
+        return y;
+    }
+    
     public void resultado() {
         System.out.println("Las coordenadas son: (" + x + ", " + y + ")");
     }
 }
+
