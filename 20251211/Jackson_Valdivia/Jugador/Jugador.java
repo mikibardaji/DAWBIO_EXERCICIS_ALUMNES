5b874802b89bd6aee7d63120266db2d99e912f79
Jack <119528940+Masterstt@users.noreply.github.com>
2025-12-04 18:14:54 +0100

    20251211

---
+++ 20251211/Jackson_Valdivia/Jugador/Jugador.java
@@ -0,0 +1,12 @@
+package Jugador;
+
+public class Jugador {
+    private String nom;
+    private int puntuacion;
+    private int distancia_recorrida;
+    private int energia;
+    private boolean powerup;
+    public Jugador(String nom){
+
+    }
+}
