4bcb2703cf1c820eb25e5673199206460aed1c92
Abraham Guzman <146139799+Guzmar17@users.noreply.github.com>
2025-12-11 15:27:11 +0100

    Implement Monedas class with value and methods

--- 20251211/AbrahamGuzman/objetos/Monedas.java
+++ 20251211/AbrahamGuzman/objetos/Monedas.java
@@ -9,5 +9,34 @@ package objetos;
  * @author xabiel
  */
 public class Monedas {
+     
+    private final int valor; 
+    public static final int VALOR_MAX = 100;
+    public static final int VALOR_MIN = 1;
+
     
+    public Moneda() {
+        Random rand = new Random();
+       
+        this.valor = rand.nextInt(VALOR_MAX - VALOR_MIN + 1) + VALOR_MIN; 
+        System.out.println("-> Constructor Moneda: Creada moneda con valor " + this.valor);
+    }
+
+    
+    public boolean hasPowerUp(int monedasRecogidas) {
+        System.out.print("-> Método Moneda.hasPowerUp(): Monedas recogidas: " + monedasRecogidas);
+        if (monedasRecogidas % 3 == 0) {
+            System.out.println(". ¡Es múltiplo de 3!");
+            return true;
+        } else {
+            System.out.println(". No es múltiplo de 3.");
+            return false;
+        }
+    }
+
+    
+    public int getValor() {
+        return valor; 
+    }
 }
+
