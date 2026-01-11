772e013fd034dddf50e07877284df7283fb63cac
RedSorsha <jovenlector@gmail.com>
2026-01-11 11:13:21 +0100

    Add log message for corassa activation in Aigua.java

--- 20260112/MoyanoSonia/Objectes/Aigua.java
+++ 20260112/MoyanoSonia/Objectes/Aigua.java
@@ -20,6 +20,7 @@ this.corassa=3;
 protected double aplicarAtributs (double dany){
 if (corassa > 0) {
         corassa--;
+        System.out.println("Corassa de " + getNom() + " activada, no es produeix dany");
         return 0;
     }
 return dany;
