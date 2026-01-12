74709657f4dd7e129b9b41fb815a0ac429652ad1
Abraham Guzman <146139799+Guzmar17@users.noreply.github.com>
2026-01-12 17:09:08 +0100

    Refactor agua class to PokemonAigua with attributes

--- 20260112/AbrahamGuzman/objetos/agua.java
+++ 20260112/AbrahamGuzman/objetos/agua.java
@@ -8,6 +8,49 @@ package objetos;
  *
  * @author xabiel
  */
-public class agua extends pokemon{
-    
+public class PokemonAigua extends Pokemon{
+    private int corassa;
+
+    public PokemonAigua() {
+        this(Nombres.Pikachu);
+    }
+
+    public PokemonAigua(Nombres nom) {
+        super(nom, 35);
+        this.corassa = 3;
+    }
+
+    public int getCorassa() {
+        return corassa;
+    }
+
+    @Override
+    public String getCaracteristiquesEspecifiques() {
+        return "Tipus: Aigua, Corassa: " + corassa;
+    }
+  
+     
+    @Override
+    public void recibirImpacto(Pokemon atacante) {
+        int dany = atacante.atac();
+        
+        
+        // Agua recibe 75% de daño si el atacante es Veneno
+        if (atacante instanceof PokemonVeri) {
+            dany = (int)(dany * 0.75);
+        }
+
+        if (corassa > 0) {
+            corassa--;
+            System.out.println(getNom() + " té corassa! No rep dany. Corassa restant: " + corassa);
+        } else {
+            lifePoints -= dany;
+            if (lifePoints <= 0) {
+                lifePoints = 0;
+                viu = false;
+            }
+        }
+
+        mostrarEstadoPokemon();
+    }
 }
