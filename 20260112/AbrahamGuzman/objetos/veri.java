4f7374e2b6a13a1b2fb558c33dc5571571d7bef7
Abraham Guzman <146139799+Guzmar17@users.noreply.github.com>
2026-01-12 17:08:06 +0100

    Implement PokemonVeri class with defense mechanics

--- 20260112/AbrahamGuzman/objetos/veri.java
+++ 20260112/AbrahamGuzman/objetos/veri.java
@@ -8,6 +8,50 @@ package objetos;
  *
  * @author xabiel
  */
-public class veri {
-    
+public class PokemonVeri extends Pokemon{
+    private boolean defensa;
+
+    public PokemonVeri() {
+        this(Nombres.Pikachu);
+    }
+
+    public PokemonVeri(Nombres nom) {
+        super(nom, 50);
+        this.defensa = false;
+    }
+
+    public boolean isDefensa() {
+        return defensa;
+    }
+
+    @Override
+    public String getCaracteristiquesEspecifiques() {
+        return "Tipus: Verí, Defensa activa: " + defensa;
+    }
+ 
+    @Override
+    public void recibirImpacto(Pokemon atacante) {
+            int dany = atacante.atac();
+        
+        // Veneno recibe 75% de daño si el atacante es Eléctrico
+        if (atacante instanceof PokemonElectric) {
+            dany = (int)(dany * 0.75);
+        }
+
+        if (defensa) {
+            dany = dany / 2;
+            System.out.println(getNom() + " té la defensa activa! Dany reduït a la meitat.");
+        }
+
+        lifePoints -= dany;
+        if (lifePoints <= 0) {
+            lifePoints = 0;
+            viu = false;
+        } else if (lifePoints < 15 && !defensa) {
+            defensa = true;
+            System.out.println(getNom() + " ha activat la defensa!");
+        }
+
+        mostrarEstadoPokemon();
+    }
 }
