af5073e25ecccdfbaed14fd44383249a6818758e
Abraham Guzman <146139799+Guzmar17@users.noreply.github.com>
2026-01-12 17:09:41 +0100

    Refactor electric class to PokemonElectric with features

--- 20260112/AbrahamGuzman/objetos/electric.java
+++ 20260112/AbrahamGuzman/objetos/electric.java
@@ -8,6 +8,50 @@ package objetos;
  *
  * @author xabiel
  */
-public class electric {
-    
+public class PokemonElectric extends Pokemon{
+
+    private int superAtac;
+
+    public PokemonElectric() {
+        this(Nombres.Pikachu);
+    }
+
+    public PokemonElectric(Nombres nom) {
+        super(nom, 40);
+        this.superAtac = 2;
+    }
+
+    public int getSuperAtac() {
+        return superAtac;
+    }
+
+    @Override
+    public String getCaracteristiquesEspecifiques() {
+        return "Tipus: Elèctric, Super Atac: " + superAtac;
+    }
+
+    @Override
+    public int atac() {
+        int forca = random.nextInt(11) + superAtac;
+        System.out.println(getNom() + " ataca amb una força de " + forca + " (inclou +" + superAtac + " de super atac)");
+        superAtac++;
+        return forca;
+    }
+    @Override
+    public void recibirImpacto(Pokemon atacante) {
+         int dany = atacante.atac();
+        
+        // Eléctrico recibe 75% de daño si el atacante es Agua
+        if (atacante instanceof PokemonAigua) {
+            dany = (int)(dany * 0.75);
+        }
+
+        lifePoints -= dany;
+        if (lifePoints <= 0) {
+            lifePoints = 0;
+            viu = false;
+        }
+
+        mostrarEstadoPokemon();
+    }
 }
