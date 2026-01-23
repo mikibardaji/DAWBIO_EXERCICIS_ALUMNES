50deb7b41770ef253782cc03f33ff5f7a6243a6e
RedSorsha <jovenlector@gmail.com>
2026-01-23 16:43:26 +0100

    Add Ex4A.java to count positive and negative numbers
    Implement a Java program to count positive and negative numbers in an array.

---
+++ 20260122/MoyanoSonia/Ex4A.java
@@ -0,0 +1,28 @@
+
+import java.util.Random;
+
+public class ex4A {
+  public static void main(String[] args) throws Exception {
+    int [] numero; //declaració de vector
+    int positiu=0, negatiu=0;
+    numero = new int [20];
+
+for (int i=0;i<numero.length;i++){
+        Random rd= new Random();
+        numero[i]=rd.nextInt(-10,11);
+       } 
+
+    for (int i=0;i<numero.length;i++){
+        if (numero[i]>0){
+            positiu++;
+        }
+        if (numero[i]<0){
+            negatiu++;
+        }
+    }
+    
+    System.out.println("Números positius:" + positiu);
+    System.out.println("Números negatius:"+ negatiu);
+
+}
+}
