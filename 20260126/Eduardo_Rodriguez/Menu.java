be406537d1ba4caf50bfbc0be8f8468042fe86be
zreduardo25-netizen <zreduardo25@gmail.com>
2026-01-29 15:21:32 +0100

    Refactor Menu class and update input methods
    Refactor Menu class to improve structure and readability. Updated methods for displaying and introducing values.

--- 20260126/Eduardo_Rodriguez/Menu.java
+++ 20260126/Eduardo_Rodriguez/Menu.java
@@ -1,75 +1,68 @@
 package objetos;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Menu {
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         int[] numeros = new int[10];
-        char opcion;
 
-        do {
-            System.out.println("a) Mostrar valores");
-            System.out.println("b) Introducir valor");
-            System.out.println("c) Salir");
-            opcion = sc.next().charAt(0);
+        boolean terminar = true;
+        char letra;
+        while (terminar == true) {
+            System.out.println("");
+            System.out.println("a. Mostrar valores");
+            System.out.println("b. Introducir valor.");
+            System.out.println("c. Salir.");
 
-            if (opcion == 'a') {
-                mostrarValores(numeros);
+            letra = sc.nextLine().charAt(0);
 
-            } else if (opcion == 'b') {
-                introducirValor(numeros, sc);
-
-            } else if (opcion == 'c') {
-                System.out.println("Programa finalizado.");
-
-            } else {
-                System.out.println("Opción no válida.");
+            if (letra == 'a') {
+                Mostrar(numeros);
+            }
+            else if (letra == 'b') {
+                introducir(numeros);
+            }
+            else if (letra == 'c') {
+                terminar = false;
             }
 
-        } while (opcion != 'c');
-
-        sc.close();
+        }
+        
     }
 
-    // -------- MÉTODOS --------
-
-    private static void mostrarValores(int[] array) {
-        System.out.println("\nContenido del array:");
-        for (int i = 0; i < array.length; i++) {
-            System.out.println("Posición " + (i + 1) + ": " + array[i]);
+    public static void Mostrar(int[] numeros) {
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Posición " + i + ": " + numeros[i]);
         }
     }
 
-    private static void introducirValor(int[] array, Scanner sc) {
+    public static void introducir(int[] numeros) {
+        Scanner sc = new Scanner(System.in);
 
-        int posicion;
+        System.out.println("Dime el numero a introducir");
+        int intro = sc.nextInt();
 
-        // Validación de posición
-        do {
-            System.out.print("Introduce la posición (1 a 10): ");
-            posicion = sc.nextInt();
-        } while (posicion < 1 || posicion > array.length);
+        System.out.println("Dime la posicion en la que lo vas a introducir (1 - 10)");
+        int posi = sc.nextInt();
+        posi -= 1;
+        if (numeros[posi] != 0) {
+            System.out.println("Ya hay un numero guardado en esa posicion");
+            System.out.println("Seguro que quieres sobreescribir? si = s / no = n ");
+            
+            char conf = sc.nextLine().charAt(0);
 
-        int indice = posicion - 1;
-
-        // Comprobar si ya hay valor
-        if (array[indice] != 0) {
-            System.out.print("Esa posición ya tiene un valor. ¿Deseas sobrescribirlo? (s/n): ");
-            char respuesta = sc.next().toLowerCase().charAt(0);
-
-            if (respuesta != 's') {
-                System.out.println("Operación cancelada.");
-                return;
+            if (conf == 's') {
+                numeros[posi] = intro;
+            }
+            else {
+                System.out.println("No se va a sobreescribir");
             }
         }
-
-        System.out.print("Introduce el valor: ");
-        int valor = sc.nextInt();
-        array[indice] = valor;
-
-        System.out.println("Valor insertado correctamente.");
+        else {
+            numeros[posi] = intro;
+        }
     }
 }
