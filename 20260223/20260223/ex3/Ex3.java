150d7d6c21bc4f24fbfc4c41dca07ddc0fec20bc
Irsis Sánchez <irsissosasanchez@gmail.com>
2026-02-26 15:57:31 +0100

    Refactor Ex3.java and add Ejercicio4 class

--- 20260223/20260223/ex3/Ex3.java
+++ 20260223/20260223/ex3/Ex3.java
@@ -2,19 +2,45 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package ex3;
+package ex4;
 
 /**
  *
  * @author iso7500
  */
-public class Ex3 {
+import java.util.Random;
 
-    /**
-     * @param args the command line arguments
-     */
+public class Ejercicio4 {
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        
+        int valor1 = rd.nextInt(1,101);
+        int[ ] datos = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            datos[i] = rd.nextInt(1,11);
+        }
+        
+        System.out.print("Vector de tamany " + n + ".");
+        
+        int pos = 0;
+        
+        while (pos >= 0) {
+            try {
+                System.out.print("Quina posició vols buscar?: ");
+                pos = entrada.nextInt();
+                
+                if (pos >= 0) {
+                    System.out.println("En la " + pos + " hay un " + datos[pos]);
+                }
+                
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Error: Aquesta posició no existeix.);
+                e.printStackTrace();
+            } catch (InputMismatchException e) {
+                System.out.println("Error: Fica-hi un valoe enter. .");
+                e.printStackTrace();           
+            }
+        }
     }
-    
 }
